39768a8bd8e577d08da44e6fa90646518e73bc20
artemklishch <artemklishch@gmail.com>
2021-08-12 08:58:17 +0300

    gfhgfdhfdhfgdh

---
+++ 11deepOOP/pr1/app.js
@@ -0,0 +1,23 @@
+// class Person {
+//   name = "Bob";
+//   constructor() {
+//     this.age = 30;
+//   }
+//   greet() {
+//     console.log(
+//       "Hi, my name is " + this.name + " and I am " + this.age + " yeasr old."
+//     );
+//   }
+// }
+
+function Person() {
+  this.name = "Bob";
+  this.age = 30;
+  this.greet = function () {
+    console.log(
+      "Hi, my name is " + this.name + " and I am " + this.age + " yeasr old."
+    );
+  };
+}
+const person = new Person();
+person.greet();
