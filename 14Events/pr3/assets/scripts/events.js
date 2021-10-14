1332296b23a1bb436700b2dcdebc2928255301a9
Artem Klishch <artemklishch@gmail.com>
2021-10-14 08:58:08 +0300

    fdgfdsgsdfgsdfg

--- 14Events/pr3/assets/scripts/events.js
+++ 14Events/pr3/assets/scripts/events.js
@@ -1,16 +1,16 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll("button");
 
 // button.onclick = function() {
 
 // };
 
-const buttonClickHandler = event => {
+const buttonClickHandler = (event) => {
   // event.target.disabled = true;
   console.log(event);
 };
 
 const anotherButtonClickHandler = () => {
-  console.log('This was clicked!');
+  console.log("This was clicked!");
 };
 
 // button.onclick = buttonClickHandler;
@@ -24,10 +24,26 @@ const boundFn = buttonClickHandler.bind(this);
 //   button.removeEventListener('click', buttonClickHandler);
 // }, 2000);
 
-buttons.forEach(btn => {
-  btn.addEventListener('mouseenter', buttonClickHandler);
+// buttons.forEach(btn => {
+//   btn.addEventListener('mouseenter', buttonClickHandler);
+// });
+
+// window.addEventListener('scroll', event => {
+//   console.log(event);
+// });
+
+// const form = document.querySelector("form");
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   console.log("event", event);
+// });
+
+const div = document.querySelector("div");
+div.addEventListener("click", (event) => {
+  console.log("div click", event);
 });
 
-window.addEventListener('scroll', event => {
-  console.log(event);
+const button = document.querySelector("button");
+button.addEventListener("click", (event) => {
+  console.log("btn click", event);
 });
