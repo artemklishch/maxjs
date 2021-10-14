9bf639761ce0b2bc81f95934896db4d9fcb50374
Artem Klishch <artemklishch@gmail.com>
2021-10-14 22:49:37 +0300

    dfgdfggfdfg

--- 14Events/pr3/assets/scripts/events.js
+++ 14Events/pr3/assets/scripts/events.js
@@ -32,18 +32,54 @@ const boundFn = buttonClickHandler.bind(this);
 //   console.log(event);
 // });
 
-// const form = document.querySelector("form");
-// form.addEventListener("submit", (event) => {
-//   event.preventDefault();
-//   console.log("event", event);
-// });
-
-const div = document.querySelector("div");
-div.addEventListener("click", (event) => {
-  console.log("div click", event);
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("event", event);
 });
 
+const div = document.querySelector("div");
+div.addEventListener(
+  "click",
+  (event) => {
+    console.log("div click", event);
+  }
+  // true
+);
+
 const button = document.querySelector("button");
-button.addEventListener("click", (event) => {
+button.addEventListener("click", function (event) {
+  event.stopPropagation();
   console.log("btn click", event);
+  console.log("this", this);
+});
+
+// const div = document.querySelector("div");
+// div.addEventListener(
+//   "mouseenter",
+//   (event) => {
+//     console.log("div click", event);
+//   },
+//   // true
+// );
+
+// const button = document.querySelector("button");
+// button.addEventListener("mouseenter", (event) => {
+//   event.stopPropagation();
+//   console.log("btn click", event);
+// });
+
+// const listItems = document.querySelectorAll("li");
+// listItems.forEach((item) =>
+//   item.addEventListener("click", (e) => {
+//     e.target.classList.toggle("hightlight");
+//   })
+// );
+
+const list = document.querySelector("ul");
+list.addEventListener("click", function (e) {
+  // e.target.classList.toggle("hightlight");
+  e.target.closest("li").classList.toggle("hightlight");
+  // form.submit();
+  console.log("this list", this);
 });
