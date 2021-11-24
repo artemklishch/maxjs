e49c5b7619ac5704dd275b0b92a9fe4be7574034
Artem Klishch <artemklishch@gmail.com>
2021-11-24 22:47:26 +0200

    dsfgsdfgdfg

---
+++ 24practise/pr1/src/SharePlace.js
@@ -0,0 +1,52 @@
+import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
+
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+    locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
+    addressForm.addEventListener("submit", this.findAddressHandler.bind(this));
+  }
+  selectPlace(coordinates) {
+    if (this.map) {
+      this.map.render(coordinates);
+    } else {
+      this.map = new Map(coordinates);
+    }
+  }
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        "Location feature is not available in your browser - please use a more modern browser or enter an address manually"
+      );
+      return;
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
+    modal.show();
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        modal.hide();
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+          // + Math.random() * 50 - исключает правильные координаты, здесь это от автора,
+          // к-й не хочет раскрывать свои координаты
+        };
+        this.selectPlace(coordinates);
+      },
+      (error) => {
+        modal.hide();
+        alert(
+          "Could not locate you unfortunately. Please enter an addres manually!"
+        );
+      }
+    );
+  }
+  findAddressHandler() {}
+}
+
+const placeFinder = new PlaceFinder();
