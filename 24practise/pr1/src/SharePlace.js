fcf3519e06f570f380361cf4a15b50826ee6fc10
Artem Klishch <artemklishch@gmail.com>
2021-11-30 00:14:21 +0200

    dfgdfgsd

--- 24practise/pr1/src/SharePlace.js
+++ 24practise/pr1/src/SharePlace.js
@@ -1,19 +1,45 @@
 import { Modal } from "./UI/Modal";
 import { Map } from "./UI/Map";
+import { getCoordsFromAddress, getAddressFromCoords } from "./Utility/Location";
 
 class PlaceFinder {
   constructor() {
     const addressForm = document.querySelector("form");
     const locateUserBtn = document.getElementById("locate-btn");
+    this.shareBtn = document.getElementById("share-btn");
     locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
+    this.shareBtn.addEventListener("click", this.sharePlaceHandler);
     addressForm.addEventListener("submit", this.findAddressHandler.bind(this));
   }
-  selectPlace(coordinates) {
+  sharePlaceHandler() {
+    const sharedLinkInputElement = document.getElementById("share-link");
+    if (!navigator.clipboard) {
+      sharedLinkInputElement.select();
+      return;
+    }
+    navigator.clipboard
+      .writeText(sharedLinkInputElement.value)
+      .then(() => {
+        alert("Copied into clipboard");
+      })
+      .catch((err) => {
+        console.log(err);
+        sharedLinkInputElement.select();
+      });
+  }
+  selectPlace(coordinates, address) {
     if (this.map) {
       this.map.render(coordinates);
     } else {
       this.map = new Map(coordinates);
     }
+    this.shareBtn.disabled = false;
+    const sharedLinkInputElement = document.getElementById("share-link");
+    sharedLinkInputElement.value = `${
+      location.origin
+    }/my-place?address=${encodeURI(address)}&lat=${coordinates.lat}&lng=${
+      coordinates.lng
+    }`;
   }
   locateUserHandler() {
     if (!navigator.geolocation) {
@@ -28,15 +54,16 @@ class PlaceFinder {
     );
     modal.show();
     navigator.geolocation.getCurrentPosition(
-      (successResult) => {
-        modal.hide();
+      async (successResult) => {
         const coordinates = {
           lat: successResult.coords.latitude,
           lng: successResult.coords.longitude,
           // + Math.random() * 50 - исключает правильные координаты, здесь это от автора,
           // к-й не хочет раскрывать свои координаты
         };
-        this.selectPlace(coordinates);
+        const address = await getAddressFromCoords(coordinates);
+        modal.hide();
+        this.selectPlace(coordinates, address);
       },
       (error) => {
         modal.hide();
@@ -46,7 +73,26 @@ class PlaceFinder {
       }
     );
   }
-  findAddressHandler() {}
+  async findAddressHandler(event) {
+    event.preventDefault();
+    const address = event.target.querySelector("input").value;
+    if (!address || address.trim().length === 0) {
+      alert("Entered address is invalid");
+      return;
+    }
+    const modal = new Modal(
+      "loading-modal-content",
+      "Loading location - please wait!"
+    );
+    modal.show();
+    try {
+      const coordinates = await getCoordsFromAddress(address);
+      this.selectPlace(coordinates, address);
+    } catch (err) {
+      alert(err.message);
+    }
+    modal.hide();
+  }
 }
 
 const placeFinder = new PlaceFinder();
